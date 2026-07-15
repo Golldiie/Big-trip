07fd6ae4b688e15491d862ee4fe93194a94f31fd
Zlata <shklyayeva.zlata@mail.ru>
2026-07-15 12:37:49 +0300

    связывает model и presenter с точкой входа

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
-import TripModel from './model/trip-model';
-import MainPresenter from './presenter/main-presenter';
+import TripModel from './model/trip-model.js';
+import MainPresenter from './presenter/main-presenter.js';
 
 const filtersContainer = document.querySelector('.trip-controls__filters');
 const eventsContainer = document.querySelector('.trip-events');
 
 const tripModel = new TripModel();
 
-const presenter = new MainPresenter({filtersContainer, eventsContainer});
+const presenter = new MainPresenter({
+  filtersContainer,
+  eventsContainer,
+  tripModel,
+});
 
 presenter.init();
--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -4,6 +4,7 @@ import Filter from '../view/filter.js';
 import FormCreation from '../view/form-creation.js';
 import RoutePoint from '../view/route-point.js';
 
+
 export default class MainPresenter {
   constructor({filtersContainer, eventsContainer, tripModel}) {
     this.filtersContainer = filtersContainer;
@@ -18,7 +19,8 @@ export default class MainPresenter {
     pointsList.classList.add('trip-events__list');
 
     this.eventsContainer.append(pointsList);
-    render(new FormCreation(), pointsList, RenderPosition.AFTERBEGIN);
+    render(new FormCreation({offers: this.tripModel.getOfferByType('flight').offers, destinations: this.tripModel.getDestinations()}), pointsList, RenderPosition.AFTERBEGIN);
+
     const points = this.tripModel.getPoints();
     points.forEach((point) => render(new RoutePoint(point), pointsList));
   }
