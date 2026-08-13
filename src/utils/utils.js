e8dc919393041e6242d3b6bf6cdb68f1fd4e0d74
Zlata <shklyayeva.zlata@mail.ru>
2026-08-13 13:04:39 +0300

    Выклатывет на github

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,15 +3,6 @@ import duration from 'dayjs/plugin/duration';
 
 dayjs.extend(duration);
 
-export const getRandomInteger = (a,b) => {
-  const lower = Math.ceil(Math.min(a,b));
-  const upper = Math.floor(Math.max(a,b));
-  const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-};
-
-export const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
 export const capitalize = (str) => {
   if (!str) {
     return str;
